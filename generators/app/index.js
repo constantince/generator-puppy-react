24eac0aada5a1deb1849208ef8b5237596d8447f
陈焱 <alberteinstein007@126.com>
2020-11-27 10:24:10 +0800

    feat: new dest path changed

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,13 +1,27 @@
 const Generator = require("yeoman-generator");
 
 module.exports = class React extends Generator {
+
+    async prompting () {
+        const prompts = [
+            {
+              type: 'input',
+              name: 'name',
+              message: 'Entre your react project name',
+              default: 'noop'
+            }
+        ];
+
+        this.answer = await this.prompt(prompts);
+    }
+
     //覆盖询问条件
     writing() {
-        console.log('writing to ', this.destinationPath(), this.templatePath());
+        // console.log('writing to ', this.destinationPath(), this.templatePath());
         // this.destinationRoot(this.destinationPath(name));
         this.fs.copyTpl(
             this.templatePath(),
-            this.destinationPath(),
+            this.destinationPath(this.answer.name),
             {},
             null,
             { globOptions: { dot: true } }
